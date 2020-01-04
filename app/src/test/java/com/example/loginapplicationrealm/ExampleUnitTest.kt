d5bfda7156ac5c03932da8ec1bf3be280ddaf2e1
PawanRoy <pawanroy0066@gmail.com>
2020-01-04 23:29:32 +0530

    Validators Tested

--- app/src/test/java/com/example/loginapplicationrealm/ExampleUnitTest.kt
+++ app/src/test/java/com/example/loginapplicationrealm/ExampleUnitTest.kt
@@ -2,16 +2,36 @@ package com.example.loginapplicationrealm
 
 import org.junit.Test
 
-import org.junit.Assert.*
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun correctNameTest(){
+        when{
+            ("This is not intended to pass this test").isValidUsername() -> assert(false)
+            ("shor").isValidUsername() -> assert(false)
+            ("Iamthecool").isValidUsername() -> assert(true)
+        }
+    }
+
+    @Test
+    fun passwordValidatorTest(){
+        when {
+            ("123456").isValidPassword() -> assert(false)
+            ("1234567891011121314").isValidPassword() -> assert(false)
+            ("123456789101112").isValidPassword() -> assert(true)
+        }
+    }
+
+    private fun String.isValidPassword(): Boolean {
+        return this.length in 8..15
+    }
+
+    private fun String.isValidUsername(): Boolean {
+        return this.length in 5..25
     }
 }
